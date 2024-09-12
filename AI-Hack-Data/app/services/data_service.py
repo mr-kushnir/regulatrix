a7181ab88e2fc4f7df0ad6d4e58c2de1a42e2284
NeDognal <95420598+NeDognal@users.noreply.github.com>
2024-09-12 15:08:36 +0300

    Update data_service.py
    Добавил исполнение Take_text, вроде верно )

--- AI-Hack-Data/app/services/data_service.py
+++ AI-Hack-Data/app/services/data_service.py
@@ -4,7 +4,13 @@ from fastapi import UploadFile
 
 
 class DataService:
+    def __init__(self, output_folder):
+        self.output_folder = output_folder
+        self.take_text = TakeText(output_folder)
 
-    async def process_data_files(self, files: [UploadFile]) -> None:
+    async def process_data_files(self, files: List[UploadFile]) -> None:
         for file in files:
-            print(file.filename)
+            if file.filename.endswith(".pdf"):
+                await self.take_text.process(file)
+            else:
+                print(f"Файл {file.filename} не является PDF-файлом и не будет обработан.")
